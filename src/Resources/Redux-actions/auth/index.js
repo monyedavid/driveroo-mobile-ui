1bc477d40fcd71fdcdd2a55636093bc32b0fa249
Joshua Oluikpe <joshuaoluikpe@gmail.com>
2019-08-12 18:51:00 +0100

    setting up | otp actions

---
+++ src/Resources/Redux-actions/auth/index.js
@@ -0,0 +1,1 @@
+import * as rp from "request-promise";
--- src/Resources/Redux-store/index.js
+++ src/Resources/Redux-store/index.js
@@ -8,12 +8,11 @@ const initialState = {};
 const middleware = [thunk];
 // const middleware = {}
 
-// create Store
-const store = createStore(
-    rootReducer, // all combined reducers
-    initialState, // DUHH initaial state at craete store level
-    composeWithDevTools(applyMiddleware(...middleware)),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default store;
+export default createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(...middleware))
+);
