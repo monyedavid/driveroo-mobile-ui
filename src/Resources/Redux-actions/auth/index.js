351333a0ee9756a3124159fb13b2a19b093a3be5
Joshua Oluikpe <joshuaoluikpe@gmail.com>
2019-08-13 03:57:48 +0100

    switch to axios

--- src/Resources/Redux-actions/auth/index.js
+++ src/Resources/Redux-actions/auth/index.js
@@ -1,9 +1,9 @@
-import { g_Auth } from "../../../Graphql/auth.graphql";
+// import { g_Auth } from "../../../Graphql/auth.graphql";
 
 export const userLogin = ({ emailormobile, password }) => async dispatch => {
-    const service = new g_Auth();
+    ///  const service = new g_Auth();
     try {
-        await service.login({ emailormobile, password });
+        // await service.login({ emailormobile, password });
     } catch (error) {
         console.log("err |", error);
     }
