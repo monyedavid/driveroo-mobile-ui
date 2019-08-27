f68d0801aa12daeb6d619e1fb1a7ce29b303b1a7
Joshua Oluikpe <joshuaoluikpe@gmail.com>
2019-08-27 16:59:13 +0100

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,82 +13,86 @@ import ConfirmationScreen from "../screens/ConfirmationScreen";
 import ResetPassword from "../screens/ResetPassword";
 import PassWordScreen from "../screens/PassWord";
 import Status from "../screens/user/Status";
+import Home from "../screens/user/Home";
 import {
-    createStackNavigator,
-    createAppContainer,
-    createDrawerNavigator
+	createStackNavigator,
+	createAppContainer,
+	createDrawerNavigator,
 } from "react-navigation";
 
 const AppNavigator = createStackNavigator(
-    {
-        // Board: {
-        //     screen: OnBoardScreen
-        // },
-        // Home: {
-        //     screen: HomeScreen
-        // },
-        // SignUp: {
-        //     screen: SignUp
-        // },
-        // OTP: {
-        //     screen: OtpVerifications
-        // },
-        // PassWord: {
-        //     screen: PassWordScreen
-        // },
-        // SocialLogin: {
-        //     screen: SocialLogin
-        // },
-        // Profile: {
-        //     screen: Profile
-        // },
-        // Verification: {
-        //     screen: VerificationScreen
-        // },
-        // ResetPassword: {
-        //     screen: ResetPassword
-        // },
-        // Confirmation: {
-        //     screen: ConfirmationScreen
-        // },
-        Status: {
-            screen: Status
-        }
-    },
-    {
-        // initialRouteName: "Profile",
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: "#fff",
-                borderBottomColor: "#f3f3f3",
-                borderBottomWidth: 4
-            },
-            headerTitleStyle: {
-                fontWeight: "bold",
-                fontSize: 17,
-                color: "#002257"
-            }
-        }
-    }
+	{
+		// Board: {
+		//     screen: OnBoardScreen
+		// },
+		Landing: {
+			screen: HomeScreen,
+		},
+		SignUp: {
+			screen: SignUp,
+		},
+		OTP: {
+			screen: OtpVerifications,
+		},
+		PassWord: {
+			screen: PassWordScreen,
+		},
+		SocialLogin: {
+			screen: SocialLogin,
+		},
+		Profile: {
+			screen: Profile,
+		},
+		Verification: {
+			screen: VerificationScreen,
+		},
+		ResetPassword: {
+			screen: ResetPassword,
+		},
+		Confirmation: {
+			screen: ConfirmationScreen,
+		},
+		Status: {
+			screen: Status,
+		},
+		Home: {
+			screen: Home,
+		},
+	},
+	{
+		// initialRouteName: "Profile",
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: "#fff",
+				borderBottomColor: "#f3f3f3",
+				borderBottomWidth: 4,
+			},
+			headerTitleStyle: {
+				fontWeight: "bold",
+				fontSize: 17,
+				color: "#002257",
+			},
+		},
+	},
 );
 
 const DrawerNavigator = createDrawerNavigator(
-    {
-        App: {
-            screen: AppNavigator
-        }
-    },
-    {
-        hideStatusBar: true,
-        drawerBackgroundColor: "rgba(255,255,255,.9)",
-        // overlayColor: "#6b52ae",
-        drawerWidth: 350,
-        contentComponent: Drawer,
-        contentOptions: {
-            activeTintColor: "#fff",
-            activeBackgroundColor: "#6b52ae"
-        }
-    }
+	{
+		App: {
+			screen: AppNavigator,
+		},
+	},
+	{
+		hideStatusBar: true,
+		drawerBackgroundColor: "rgba(255,255,255,.9)",
+		// overlayColor: "#6b52ae",
+		drawerWidth: 350,
+		contentComponent: Drawer,
+		contentOptions: {
+			activeTintColor: "#fff",
+			activeBackgroundColor: "#6b52ae",
+		},
+	},
 );
 
 export default createAppContainer(DrawerNavigator);
--- src/routes/menu.js
+++ src/routes/menu.js
@@ -5,34 +5,34 @@ import Reviews from "../assets/images/menu/reviews.png";
 import Settings from "../assets/images/menu/settings.png";
 
 export const menuArray = [
-    {
-        id: 1,
-        screen: "Status",
-        title: "Earnings",
-        icon: Earnings
-    },
-    {
-        id: 2,
-        screen: "Status",
-        title: " Driving History",
-        icon: Driving
-    },
-    {
-        id: 3,
-        screen: "Status",
-        title: " Reviews and Help",
-        icon: Reviews
-    },
-    {
-        id: 4,
-        screen: "Status",
-        title: " Bonuses",
-        icon: Bonuses
-    },
-    {
-        id: 5,
-        screen: "Status",
-        title: " Settings",
-        icon: Settings
-    }
+	{
+		id: 1,
+		screen: "Status",
+		title: "Earnings",
+		icon: Earnings,
+	},
+	{
+		id: 2,
+		screen: "Home",
+		title: " Driving History",
+		icon: Driving,
+	},
+	{
+		id: 3,
+		screen: "Status",
+		title: " Reviews and Help",
+		icon: Reviews,
+	},
+	{
+		id: 4,
+		screen: "Status",
+		title: " Bonuses",
+		icon: Bonuses,
+	},
+	{
+		id: 5,
+		screen: "Status",
+		title: " Settings",
+		icon: Settings,
+	},
 ];
