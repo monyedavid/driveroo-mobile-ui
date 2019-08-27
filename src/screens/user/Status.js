9e7ed4a88d7c01ac57348afc6a05447964088a58
Joshua Oluikpe <joshuaoluikpe@gmail.com>
2019-08-27 15:36:13 +0100

    .

--- src/screens/user/Status.js
+++ src/screens/user/Status.js
@@ -8,7 +8,6 @@ import "../../styles/core/utilis";
 import { Icon } from "native-base";
 
 class Profile extends Component {
-<<<<<<< HEAD
 	state = {
 		firstName: "",
 		lastname: "",
@@ -153,200 +152,70 @@ class Profile extends Component {
 	}
 }
 
-=======
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text
-                    style={[
-                        utilis.text_sm,
-                        utilis.margin_bottom_lg,
-                        utilis.text_center
-                    ]}
-                >
-                    Account Status
-                </Text>
-
-                <View style={styles.bars}>
-                    <View style={styles.status}>
-                        <View style={styles.left}>
-                            <Image
-                                source={require("../../assets/images/id-card.png")}
-                                style={styles.image}
-                            />
-                            <Text style={styles.text_title}>
-                                Identification
-                            </Text>
-                        </View>
-
-                        <View style={styles.right}>
-                            <View style={styles.circle("yellow")}></View>
-                            <Text style={styles.text_status}>In progress</Text>
-                        </View>
-                    </View>
-
-                    <View style={styles.status}>
-                        <View style={styles.left}>
-                            <Image
-                                source={require("../../assets/images/Vector.png")}
-                                style={styles.image}
-                            />
-                            <Text style={styles.text_title}>
-                                English Language
-                            </Text>
-                        </View>
-
-                        <View style={styles.right}>
-                            <View style={styles.circle("red")}></View>
-                            <Text style={styles.text_status}>pending</Text>
-                        </View>
-                    </View>
-
-                    <View style={styles.status}>
-                        <View style={styles.left}>
-                            <Image
-                                source={require("../../assets/images/speak.png")}
-                                style={styles.image}
-                            />
-                            <Text style={styles.text_title}>
-                                Communication Text
-                            </Text>
-                        </View>
-
-                        <View style={styles.right}>
-                            <View style={styles.circle("red")}></View>
-                            <Text style={styles.text_status}>pending</Text>
-                        </View>
-                    </View>
-
-                    <View style={styles.status}>
-                        <View style={styles.left}>
-                            <Image
-                                source={require("../../assets/images/steering-wheel.png")}
-                                style={styles.image}
-                            />
-                            <Text style={styles.text_title}>
-                                Driving Ability Test
-                            </Text>
-                        </View>
-
-                        <View style={styles.right}>
-                            <View style={styles.circle("red")}></View>
-                            <Text style={styles.text_status}>pending</Text>
-                        </View>
-                    </View>
-
-                    <View style={styles.status}>
-                        <View style={styles.left}>
-                            <Image
-                                source={require("../../assets/images/verify-me.png")}
-                                style={styles.image}
-                            />
-                            <Text style={styles.text_title}>
-                                Verifyme Check
-                            </Text>
-                        </View>
-
-                        <View style={styles.right}>
-                            <View style={styles.circle("red")}></View>
-                            <Text style={styles.text_status}>pending</Text>
-                        </View>
-                    </View>
-
-                    <View style={styles.status}>
-                        <View style={styles.left}>
-                            <Image
-                                source={require("../../assets/images/onboard.png")}
-                                style={styles.image}
-                            />
-                            <Text style={styles.text_title}>
-                                Onboarding Seminar
-                            </Text>
-                        </View>
-
-                        <View style={styles.right}>
-                            <View style={styles.circle("red")}></View>
-                            <Text style={styles.text_status}>pending</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={utilis.child_container}>
-                    <Button disabled={true} title='pending...' />
-                </View>
-            </View>
-        );
-    }
-}
-
-Profile.navigationOptions = {
-    headers: null
-};
-
->>>>>>> 948df44d16f2e5848c2c62484d6bb6a1b1a112fe
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: 20
-    },
-    bars: {
-        marginBottom: 10
-    },
-    status: {
-        flexDirection: "row",
-        borderTopWidth: 1,
-        borderBottomWidth: 1,
-        borderColor: "#121B74",
-        padding: 20,
-        paddingLeft: 40,
-        paddingRight: 40,
-        marginBottom: 15
-    },
-    left: {
-        flex: 2,
-        flexDirection: "row",
-        // borderWidth: 1,
-        borderColor: "red",
-        alignItems: "center",
-        justifyContent: "flex-start"
-    },
-    right: {
-        flex: 1,
-        flexDirection: "row",
-        // borderWidth: 1,
-        alignItems: "center",
-        justifyContent: "space-around"
-    },
-    image: {
-        // width: "",
-        // resizeMode: "cover",
-        marginRight: 9
-        // borderWidth: 1,
-    },
-    text_title: {
-        color: "#121B74",
-        fontWeight: "700",
-        fontSize: 14
-        // borderWidth: 1,
-    },
-    text_status: {
-        fontSize: 13,
-        color: "#202020"
-    },
-    circle: function(color) {
-        return {
-            height: 8,
-            width: 8,
-            borderRadius: 4,
-            backgroundColor: color
-        };
-    }
+	container: {
+		flex: 1,
+		marginTop: 20,
+	},
+	bars: {
+		marginBottom: 10,
+	},
+	status: {
+		flexDirection: "row",
+		borderTopWidth: 1,
+		borderBottomWidth: 1,
+		borderColor: "#121B74",
+		padding: 20,
+		paddingLeft: 40,
+		paddingRight: 40,
+		marginBottom: 15,
+	},
+	left: {
+		flex: 2,
+		flexDirection: "row",
+		// borderWidth: 1,
+		borderColor: "red",
+		alignItems: "center",
+		justifyContent: "flex-start",
+	},
+	right: {
+		flex: 1,
+		flexDirection: "row",
+		// borderWidth: 1,
+		alignItems: "center",
+		justifyContent: "space-around",
+	},
+	image: {
+		// width: "",
+		// resizeMode: "cover",
+		marginRight: 9,
+		// borderWidth: 1,
+	},
+	text_title: {
+		color: "#121B74",
+		fontWeight: "700",
+		fontSize: 14,
+		// borderWidth: 1,
+	},
+	text_status: {
+		fontSize: 13,
+		color: "#202020",
+	},
+	circle: function(color) {
+		return {
+			height: 8,
+			width: 8,
+			borderRadius: 4,
+			backgroundColor: color,
+		};
+	},
 });
 
 const map_state_to_props = state => ({
-    auth: state.auth
+	auth: state.auth,
 });
 
 export default connect(
-    map_state_to_props,
-    { userReg }
+	map_state_to_props,
+	{ userReg },
 )(Profile);
