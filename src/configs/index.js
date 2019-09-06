5757a544280cf1498388d58d051f66af250cd3b4
rasheeddavid <griffinc317@gmail.com>
2019-09-06 07:18:13 +0100

    ip address andriod

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,6 +1,8 @@
 export default {
     AUTH_MS_DEV: "http://localhost:4000",
     DRIVER_MS_dev: "http://localhost:4100",
+    AUTH_MS_DEV_ANDRIOD: "http://192.168.10.101:4000",
+    DRIEVR_MS_DEV_ANDRIOD: "http://192.168.10.101:4100",
 
     AUTH_MS: "https://driverro-auth.herokuapp.com",
     DRIVER_MS: "https://driveroo-driver-ms.herokuapp.com",
