919090519820b145fa1a9c146c0a416791473555
rasheeddavid <griffinc317@gmail.com>
2019-09-09 10:20:55 +0100

    accepte changes

--- src/apollo.js
+++ src/apollo.js
@@ -1,16 +1,17 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
+import { createUploadLink } from "apollo-upload-client";
+// import { HttpLink } from "apollo-link-http";
 import { Platform } from "react-native";
 
-const host =
-    Platform.OS === "ios" ? "http://localhost:4000" : "http://10.0.2.2:4000";
+const uri =
+    Platform.OS === "ios" ? "http://localuri:4000" : "http://10.0.2.2:4000";
+
+const link = createUploadLink({ uri });
 
 // "://localhost:4000"
 
 export const client = new ApolloClient({
-    link: new HttpLink({
-        uri: `${host}`
-    }),
+    link,
     cache: new InMemoryCache()
 });
